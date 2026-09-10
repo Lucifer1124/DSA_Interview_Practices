8cd94206aeacfe1a8426dac30d73a7afe5be3462
Puspal Biswas <163633218+Lucifer1124@users.noreply.github.com>
2026-09-10 22:05:56 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,48 @@
+class MinStack {
+    int[] stack;
+    int[] minStack;
+    int top;
+    int n;
+   
+    public MinStack() {
+       n = 9999;
+       stack = new int[n];
+       minStack = new int[n];
+       top = -1; 
+    }
+    
+    public boolean empty(){
+        return top == -1;
+    }
+    public boolean full(){
+        return top == n - 1;
+    }
+
+    public void push(int val) {
+        if(full()){
+            return;
+        }
+        stack[++top] = val;
+        if(top == 0){
+            minStack[top] = val;
+        } else{
+            minStack[top] = Math.min(val, minStack[top - 1]); 
+        }
+    }
+    
+    public void pop() {
+        if(empty()){
+            return ;
+        }
+        top--;
+        
+    }
+    
+    public int top() {
+        return stack[top];
+    }
+
+    public int getMin() {
+       return minStack[top];
+    }
+}
